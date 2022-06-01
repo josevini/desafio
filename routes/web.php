b730250f605529c8abe11baea9f593cdfffcc9cc
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-31 23:30:45 -0300

    updating routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [\App\Http\Controllers\AlbumController::class, 'listAlbums'])->name('home');
+
+Route::get('/search', [\App\Http\Controllers\AlbumController::class, 'listAlbumsWhere'])->name('search');
+
+Route::controller(\App\Http\Controllers\AlbumController::class)->group(function() {
+    Route::get('/new/album', 'getFormNewAlbum')->name('form-new-album');
+    Route::post('/add/album', 'addAlbum')->name('add-album');
+    Route::delete('/delete/album/{album}', 'deleteAlbum')->name('delete-album');
+});
+
+Route::controller(\App\Http\Controllers\MusicController::class)->group(function() {
+    Route::get('new/music', 'getFormNewMusic')->name('form-new-music');
+    Route::post('add/music', 'addMusic')->name('add-music');
 });
