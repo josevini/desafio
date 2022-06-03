7619aa13774837ba79130ec9c46c9c9cefd09eee
Vinicius Silva <ferreiravini468@gmail.com>
2022-06-02 22:49:07 -0300

    Updating forms

--- resources/views/form/new-album.blade.php
+++ resources/views/form/new-album.blade.php
@@ -1,13 +1,13 @@
 @extends('home')
 
 @section('content')
-    <form method="post" action="{{ route('add-album')  }}">
+    <form method="post" action="{{ route('add-album')  }}" class="form-new">
         @csrf
         Nome: <input type="text" name="name" required />
         Data: <input type="number" min="1900" max="2099" value="{{date('Y')}}" name="date" required/>
         <input type="submit" value="Cadastrar">
-        <a class="btn" href="{{ route('home') }}">
-            voltar
-        </a>
     </form>
+    <a class="btn" href="{{ route('home') }}">
+        voltar
+    </a>
 @endsection
--- resources/views/form/new-music.blade.php
+++ resources/views/form/new-music.blade.php
@@ -1,7 +1,7 @@
 @extends('home')
 
 @section('content')
-    <form method="post" action="{{ route('add-music')  }}">
+    <form method="post" action="{{ route('add-music')  }}" class="form-new">
         @csrf
         Nome da Música: <input type="text" name="name" required />
         Álbum: <select name="album">
@@ -11,9 +11,10 @@
                 </option>
             @endforeach
         </select>
+        Duração (segundos) <input type="number" name="duration" required>
         <input type="submit" value="Cadastrar">
-        <a class="btn" href="{{ route('home') }}">
-            voltar
-        </a>
     </form>
+    <a class="btn" href="{{ route('home') }}">
+        voltar
+    </a>
 @endsection
